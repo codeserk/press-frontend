7b81736bdd9908cce466381c44ce71c14d2005d4
Jose Camara <jose.camara@coosto.com>
2021-05-02 18:57:54 +0200

    Improve layout (just a bit)

--- client/api.ts
+++ client/api.ts
@@ -53,6 +53,37 @@ export interface CreateFieldRequest {
 /**
  * 
  * @export
+ * @interface CreateNodeRequest
+ */
+export interface CreateNodeRequest {
+    /**
+     * 
+     * @type {object}
+     * @memberof CreateNodeRequest
+     */
+    data?: object;
+    /**
+     * 
+     * @type {string}
+     * @memberof CreateNodeRequest
+     */
+    name: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof CreateNodeRequest
+     */
+    schemaId_validate?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof CreateNodeRequest
+     */
+    slug: string;
+}
+/**
+ * 
+ * @export
  * @interface CreateSchemaRequest
  */
 export interface CreateSchemaRequest {
@@ -179,6 +210,49 @@ export interface MeResponse {
 /**
  * 
  * @export
+ * @interface NodeEntity
+ */
+export interface NodeEntity {
+    /**
+     * 
+     * @type {object}
+     * @memberof NodeEntity
+     */
+    data?: object;
+    /**
+     * 
+     * @type {string}
+     * @memberof NodeEntity
+     */
+    id?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof NodeEntity
+     */
+    name?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof NodeEntity
+     */
+    realmId?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof NodeEntity
+     */
+    schemaId?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof NodeEntity
+     */
+    slug?: string;
+}
+/**
+ * 
+ * @export
  * @interface RealmEntity
  */
 export interface RealmEntity {
@@ -281,7 +355,7 @@ export interface UpdateFieldRequest {
      * @type {object}
      * @memberof UpdateFieldRequest
      */
-    data?: object;
+    config?: object;
     /**
      * 
      * @type {string}
@@ -1003,6 +1077,162 @@ export class FieldApi extends BaseAPI {
      */
     public updateField(requestParameters: FieldApiUpdateFieldRequest, options?: any) {
         return FieldApiFp(this.configuration).updateField(requestParameters.realmId, requestParameters.schemaId, requestParameters.fieldId, requestParameters.body, options).then((request) => request(this.axios, this.basePath));
+    }
+}
+
+
+/**
+ * NodeApi - axios parameter creator
+ * @export
+ */
+export const NodeApiAxiosParamCreator = function (configuration?: Configuration) {
+    return {
+        /**
+         * Creates a new node
+         * @summary Creates a new node
+         * @param {string} realmId Realm ID
+         * @param {CreateNodeRequest} [body] Node parameters
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        createNode: async (realmId: string, body?: CreateNodeRequest, options: any = {}): Promise<RequestArgs> => {
+            // verify required parameter 'realmId' is not null or undefined
+            if (realmId === null || realmId === undefined) {
+                throw new RequiredError('realmId','Required parameter realmId was null or undefined when calling createNode.');
+            }
+            const localVarPath = `/v1/realm/{realmId}/node`
+                .replace(`{${"realmId"}}`, encodeURIComponent(String(realmId)));
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, 'https://example.com');
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication ApiKeyAuth required
+            if (configuration && configuration.apiKey) {
+                const localVarApiKeyValue = typeof configuration.apiKey === 'function'
+                    ? await configuration.apiKey("Authorization")
+                    : await configuration.apiKey;
+                localVarHeaderParameter["Authorization"] = localVarApiKeyValue;
+            }
+
+
+    
+            localVarHeaderParameter['Content-Type'] = 'application/json';
+
+            const queryParameters = new URLSearchParams(localVarUrlObj.search);
+            for (const key in localVarQueryParameter) {
+                queryParameters.set(key, localVarQueryParameter[key]);
+            }
+            for (const key in options.query) {
+                queryParameters.set(key, options.query[key]);
+            }
+            localVarUrlObj.search = (new URLSearchParams(queryParameters)).toString();
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            const nonString = typeof body !== 'string';
+            const needsSerialization = nonString && configuration && configuration.isJsonMime
+                ? configuration.isJsonMime(localVarRequestOptions.headers['Content-Type'])
+                : nonString;
+            localVarRequestOptions.data =  needsSerialization
+                ? JSON.stringify(body !== undefined ? body : {})
+                : (body || "");
+
+            return {
+                url: localVarUrlObj.pathname + localVarUrlObj.search + localVarUrlObj.hash,
+                options: localVarRequestOptions,
+            };
+        },
+    }
+};
+
+/**
+ * NodeApi - functional programming interface
+ * @export
+ */
+export const NodeApiFp = function(configuration?: Configuration) {
+    return {
+        /**
+         * Creates a new node
+         * @summary Creates a new node
+         * @param {string} realmId Realm ID
+         * @param {CreateNodeRequest} [body] Node parameters
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async createNode(realmId: string, body?: CreateNodeRequest, options?: any): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<NodeEntity>> {
+            const localVarAxiosArgs = await NodeApiAxiosParamCreator(configuration).createNode(realmId, body, options);
+            return (axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
+                const axiosRequestArgs = {...localVarAxiosArgs.options, url: (configuration?.basePath || basePath) + localVarAxiosArgs.url};
+                return axios.request(axiosRequestArgs);
+            };
+        },
+    }
+};
+
+/**
+ * NodeApi - factory interface
+ * @export
+ */
+export const NodeApiFactory = function (configuration?: Configuration, basePath?: string, axios?: AxiosInstance) {
+    return {
+        /**
+         * Creates a new node
+         * @summary Creates a new node
+         * @param {string} realmId Realm ID
+         * @param {CreateNodeRequest} [body] Node parameters
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        createNode(realmId: string, body?: CreateNodeRequest, options?: any): AxiosPromise<NodeEntity> {
+            return NodeApiFp(configuration).createNode(realmId, body, options).then((request) => request(axios, basePath));
+        },
+    };
+};
+
+/**
+ * Request parameters for createNode operation in NodeApi.
+ * @export
+ * @interface NodeApiCreateNodeRequest
+ */
+export interface NodeApiCreateNodeRequest {
+    /**
+     * Realm ID
+     * @type {string}
+     * @memberof NodeApiCreateNode
+     */
+    readonly realmId: string
+
+    /**
+     * Node parameters
+     * @type {CreateNodeRequest}
+     * @memberof NodeApiCreateNode
+     */
+    readonly body?: CreateNodeRequest
+}
+
+/**
+ * NodeApi - object-oriented interface
+ * @export
+ * @class NodeApi
+ * @extends {BaseAPI}
+ */
+export class NodeApi extends BaseAPI {
+    /**
+     * Creates a new node
+     * @summary Creates a new node
+     * @param {NodeApiCreateNodeRequest} requestParameters Request parameters.
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof NodeApi
+     */
+    public createNode(requestParameters: NodeApiCreateNodeRequest, options?: any) {
+        return NodeApiFp(this.configuration).createNode(requestParameters.realmId, requestParameters.body, options).then((request) => request(this.axios, this.basePath));
     }
 }
 
--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,15 +1,15 @@
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons'
 import { Breadcrumb, Menu } from 'antd'
 import Layout, { Content, Header } from 'antd/lib/layout/layout'
 import Sider from 'antd/lib/layout/Sider'
-import SubMenu from 'antd/lib/menu/SubMenu'
 import Link from 'next/link'
 import { useContext } from 'react'
 
 import { RealmStoreContext } from '../../store/realm.store'
+import { SchemaStoreContext } from '../../store/schema.store'
 
 export function AppLayout(props) {
-  const { realms } = useContext(RealmStoreContext)
+  const { realms, currentRealm } = useContext(RealmStoreContext)
+  const { schemas, currentSchemaId, currentSchema, currentField } = useContext(SchemaStoreContext)
 
   return (
     <Layout>
@@ -30,23 +30,44 @@ export function AppLayout(props) {
 
       <Layout>
         <Sider width={200} className="site-layout-background">
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['608952068bf128992371e0bd']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100%', borderRight: 0 }}>
-            {realms.map((realm) => (
-              <Menu.Item key={realm.id}>
-                <Link href={`/realm/${realm.id}`}>{realm.name}</Link>
-              </Menu.Item>
-            ))}
-          </Menu>
+          {!currentRealm && (
+            <Menu mode="inline" style={{ height: '100%', borderRight: 0 }}>
+              {realms.map((realm) => (
+                <Menu.Item key={realm.id}>
+                  <Link href={`/realm/${realm.id}`}>{realm.name}</Link>
+                </Menu.Item>
+              ))}
+            </Menu>
+          )}
+
+          {currentRealm && (
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={[currentSchemaId]}
+              style={{ height: '100%', borderRight: 0 }}>
+              {schemas.map((schema) => (
+                <Menu.Item key={schema.id}>
+                  <Link href={`/realm/${currentRealm.id}/schema/${schema.id}`}>{schema.name}</Link>
+                </Menu.Item>
+              ))}
+            </Menu>
+          )}
         </Sider>
         <Layout style={{ padding: '0 24px 24px' }}>
           <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
+            {currentRealm && (
+              <Breadcrumb.Item>
+                <Link href={`/realm/${currentRealm.id}`}>{currentRealm.name}</Link>
+              </Breadcrumb.Item>
+            )}
+            {currentSchema && (
+              <Breadcrumb.Item>
+                <Link href={`/realm/${currentRealm.id}/schema/${currentSchema.id}`}>
+                  {currentSchema.name}
+                </Link>
+              </Breadcrumb.Item>
+            )}
+            {currentField && <Breadcrumb.Item>{currentField.name}</Breadcrumb.Item>}
           </Breadcrumb>
           <Content
             className="site-layout-background"
